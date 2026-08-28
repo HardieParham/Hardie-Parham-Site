63c55c49c8399f9a35568569537444ac21fe64ef
Hardie <hparham865@gmail.com>
2026-08-28 16:28:40 -0500

    [feature] replaced image cards with autoplay carousel

---
+++ frontend/src/scripts/main.js
@@ -0,0 +1,34 @@
+import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+
+const splide = new Splide( '.splide', {
+	type   : 'loop',
+	drag   : 'free',
+	focus  : 'center',
+	pauseOnHover: false,
+	pauseOnFocus: false,
+	lazyLoad: 'nearby',
+	perPage: 5,
+	breakpoints: {
+    	1536: {
+      		perPage: 4, // For screens smaller than 1024px
+    	},
+    	1280: {
+      		perPage: 3, // For screens smaller than 640px
+    	},
+    	500: {
+      		perPage: 2, // For screens smaller than 640px
+			autoScroll: {
+				speed: 0.75,
+			},
+    	},
+	},
+	arrows: false,
+	autoScroll: {
+		speed: 1,
+	},
+} );
+
+splide.mount( {AutoScroll} );
+console.log("Hello There!")
